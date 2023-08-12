a4adb0a6277d83f86cec9d8bd25e1e15230a9a91
mahfujarr <mahfujarrahmanjoy@gmail.com>
2023-08-12 12:07:27 +0600

    KMS

---
+++ Activator.bat
@@ -0,0 +1,6 @@
+if exist "C:\Program Files\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files\Microsoft Office\Office16"
+if exist "C:\Program Files (x86)\Microsoft Office\Office16\ospp.vbs" cd /d "C:\Program Files (x86)\Microsoft Office\Office16"
+cscript ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH
+cscript ospp.vbs /sethst:kms.teevee.asia
+cscript ospp.vbs /act
+pause
